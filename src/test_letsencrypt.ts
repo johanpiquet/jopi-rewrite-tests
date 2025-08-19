2b0771ed0e5ce3dd16afe3dbe2e15f3bb6635426
Johan PIQUET <johan.piquet@gmail.com>
2025-08-19 23:05:12 +0200

    LetsEncrypt : fallback if timeout

--- src/test_letsencrypt.ts
+++ src/test_letsencrypt.ts
@@ -2,4 +2,8 @@ import {jopiEasy} from "jopi-rewrite";
 
 jopiEasy.new_webSite("https://johan-piquet.fr")
     .add_httpCertificate().
-        generate_letsEncryptCert("mymail@gmail.com").disable_log().force_timout_sec(5).done();
+        generate_letsEncryptCert("mymail@gmail.com")
+        .disable_log()
+        .force_timout_sec(5)
+        .if_timeOutError(() => { console.log("Timed out !")})
+        .done();
