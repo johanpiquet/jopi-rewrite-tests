61aa4512bda1af64b86749dd624d62555440e515
Johan PIQUET <johan.piquet@gmail.com>
2025-08-19 22:11:19 +0200

    Added LetsEncrypt to JopiEasy

--- src/testJopiEasy/test_newWebSite.ts
+++ src/testJopiEasy/test_newWebSite.ts
@@ -1,13 +1,9 @@
 import {jopiEasy} from "jopi-rewrite";
 
-jopiEasy.new_webSite("https://127.0.0.1")
-    .add_httpCertificate().forLocalDev().done()
-    .add_fileServer("www").done().
+jopiEasy.new_fileServer("https://127.0.0.1")
+    .set_rootDir("www").done()
+    .add_httpCertificate().generate_localDevCert().done()
 
-    hook_webSite(webSite => {
-        webSite.onGET("/version", req => {
-            return req.jsonResponse({version: "1.0.0"});
-        });
-    });
-
-
+    .hook_webSite(webSite =>
+        webSite.onGET("/version", req => req.jsonResponse({version: "1.0.0"}
+    )));
--- src/test_letsencrypt.ts
+++ src/test_letsencrypt.ts
@@ -1,29 +1,5 @@
-import {JopiServer, WebSite, getLetsEncryptCertificate} from "jopi-rewrite";
+import {jopiEasy} from "jopi-rewrite";
 
-const server = new JopiServer();
-
-const myHttpsWebSite = server.addWebsite(new WebSite("https://johan-piquet.fr"));
-
-// ACME challenge requires port 80 of the server.
-const myHttpWebSite = myHttpsWebSite.createHttpRedirectWebsite();
-server.addWebsite(myHttpWebSite);
-
-server.startServer();
-
-try {
-    let certificate = await getLetsEncryptCertificate(myHttpWebSite, {
-        log: true,
-
-        email: "mymail@gmail.com",
-
-        // When not in production, it doesn't check the server.
-        isProduction: false,
-
-        timout_sec: 5
-    });
-
-    console.log("Certificate installed");
-}
-catch (e) {
-    console.error("Can't install certificat", e);
-}
+jopiEasy.new_webSite("https://johan-piquet.fr")
+    .add_httpCertificate().
+        generate_letsEncryptCert("mymail@gmail.com").disable_log().force_timout_sec(5).done();
