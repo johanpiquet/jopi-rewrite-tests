f0a8c4e8fa450fbbcbf406538b4dd2218caaa756
Johan PIQUET <johan.piquet@gmail.com>
2025-08-28 14:26:05 +0200

    Refactoring autostartstop

--- src/testBug.tsx
+++ src/testBug.tsx
@@ -3,14 +3,30 @@ import {jopiApp} from "jopi-rewrite";
 jopiApp.startApp(jopiEasy => {
     jopiEasy.new_webSite("http://127.0.0.1")
         .add_sourceServer()
-            .useOrigin("http://my-source-server-a.local")
-            .on_beforeRequesting(url => {
-                console.log("Sending a request to " + url)
-            })
-            .on_ifServerIsDown(serverReplace => {
-                console.log("Server is ko: http://my-source-server-a.local");
-                serverReplace.useOrigin("http://my-source-server-a.local").set_weight(1)
-            })
+        .useOrigin("http://my-source-server-a.local")
+
+        // Is called to start the remote server.
+        .do_startServer(async () => {
+            console.log("Starting server A");
+
+            // Start the docker.
+            await Bun.$`cd wordpressDocker; docker compose up -d`;
+
+            // Allows stopping the server after 10 minutes
+            // if not request are emitted for this server.
+            return NodeSpace.timer.ONE_MINUTE * 10;
+        })
+
+        // Allow stopping the remote server.
+        .do_stopServer(async () => {
+            console.log("Stopping server A");
+
+            // Stop the docker.
+            await Bun.$`cd wordpressDocker; docker compose down`;
+        })
+
         .END_add_sourceServer()
+
+        // directProxyToServer allows sending the request to the server.
         .add_path_GET("/**", req => req.directProxyToServer())
-    });
+});
