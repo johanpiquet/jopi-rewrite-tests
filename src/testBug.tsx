1dd0c02e7017d25934e957246d909a0939548dd1
Johan PIQUET <johan.piquet@gmail.com>
2025-08-31 12:35:56 +0200

    Doc: User Roles

--- src/testBug.tsx
+++ src/testBug.tsx
@@ -2,46 +2,21 @@ import {jopiApp} from "jopi-rewrite";
 
 jopiApp.startApp(jopiEasy => {
     jopiEasy.new_webSite("http://127.0.0.1")
-        .add_jwtTokenAuth()
-        .step_setPrivateKey("my-secret-key")
-        .step_setUserStore()
-            .use_simpleLoginPassword()
-            .addOne("usera", "pwd-usera", {id: "userA", roles: ["admin", "reader", "writer"]})
-            .addOne("userb", "pwd-userb", {id: "userb", roles: ["admin", "reader"]})
-            .addOne("userc", "pwd-userc", {id: "userc"})
-            .DONE_use_simpleLoginPassword()
-        .stepConfigure()
-            // Cookie will be valid for 4 hours.
-            // (default is 7 days)
-            .set_cookieDuration(4)
-            .DONE_stepConfigure()
-        .DONE_add_jwtTokenAuth()
     .add_path_GET("/", req => {
-        // Will get our user info or leave and
-        // returns a 401 (unauthorized) if the user
-        // is not logged in (you can use getUserInfos
-        // to avoid this behavior).
+        // If the user does not have this role, then throw
+        // a NotAuthorizedException error, which will
+        // return a response with code 401.
         //
-        let infos = req.requireUserInfos();
+        req.assertUserHasRoles("reader");
 
-        // Returns the information.
-        return req.jsonResponse(infos);
-    })
-    .add_path_GET("/auth", async req => {
-        // Will read data from the url.
-        const userLoginPassword = await req.getReqData<UserLoginPassword>();
-
-        // Auth the user. Once done, the auth token is stored in a cookie.
-        const authRes = await req.tryAuthWithJWT(userLoginPassword);
-
-        if (!authRes.isOk) {
-            return req.textResponse(authRes.errorMessage||"", 401);
+        if (req.userHasRoles("admin", "writer")) {
+            return req.htmlResponse("You are an admin with writer role!");
         }
 
-        // When creating an auth API, you must return a JSON with the token.
-        // If it's an auth page, then you have to know that "req.tryAuthWithJWT"
-        // automatically add the JWT token to the "Authentification" cookie.
-        //
-        return req.jsonResponse(authRes.authToken);
+        if (req.userHasRoles("writer")) {
+            return req.htmlResponse("You have writer access!");
+        }
+
+        return req.htmlResponse("Your roles:" + JSON.stringify(req.getUserRoles()));
     });
 });
