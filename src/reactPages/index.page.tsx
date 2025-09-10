9c867e2f4b5693215e210b4116e655c070f95c55
Johan PIQUET <johan.piquet@gmail.com>
2025-09-10 21:09:40 +0200

    * usePage: added browset part * added "useExecuteOnce"

--- src/reactPages/index.page.tsx
+++ src/reactPages/index.page.tsx
@@ -1,10 +1,17 @@
-import {isBrowserSide} from "jopi-rewrite-ui";
+import {isBrowserSide, useExecuteOnce, usePage} from "jopi-rewrite-ui";
 import {Link} from "react-router";
 import React from "react";
 
 const text = isBrowserSide() ? "Browser side component" : "Server side component";
 
 export default function() {
+    const page = usePage();
+
+    useExecuteOnce(() => {
+        console.log("executing (useExecuteOnce)");
+        page.addToHeader("link1", <link key="1" href="style2222.css" rel="stylesheet"/>)
+    });
+
     return <div>
         <div>{text}</div>
         <div><Link to="/products">Products</Link></div>
