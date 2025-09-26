49559738a7097c917f2598f07fb29233dbef764e
Johan PIQUET <johan.piquet@gmail.com>
2025-09-26 12:56:52 +0200

    Cache refactoring: * Removed meta capacities * Added cache iterator

---
+++ src/test_cacheIterator.tsx
@@ -0,0 +1,24 @@
+import {jopiApp} from "jopi-rewrite";
+
+jopiApp.startApp(jopiEasy => {
+    jopiEasy.new_webSite("http://127.0.0.1")
+        .enable_automaticCache()
+            .use_memoryCache()
+            .END_use_AutomaticCache()
+
+        .add_path_USE("/", {
+            onGET: async req => req.htmlResponse(new Date().toLocaleTimeString())
+        })
+
+        .add_path("/cache")
+            .onGET(async req => {
+                let itr = req.getCacheEntryIterator();
+
+                for (const value of itr) {
+                    console.log(value.url);
+                }
+
+                return req.htmlResponse(new Date().toLocaleTimeString())
+            })
+            .disable_automaticCache()
+});
