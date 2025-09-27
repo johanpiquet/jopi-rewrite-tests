737ff1f8181b69cbfc922efc296038f43982c925
Johan PIQUET <johan.piquet@gmail.com>
2025-09-27 18:38:45 +0200

    Tests with shadcn

--- src/reactPages/index.page.tsx
+++ src/reactPages/index.page.tsx
@@ -1,18 +1,18 @@
 import {isBrowserSide, useExecuteOnce, useUserInfos, usePage} from "jopi-rewrite-ui";
 import {Link} from "react-router";
 import React from "react";
+import {Button} from "../shadcn/components/ui/button.tsx";
 
 const text = isBrowserSide() ? "Browser side component" : "Server side component";
 
 export default function() {
     const page = usePage();
-
     const userInfos = useUserInfos();
 
     useExecuteOnce(() => {
         console.log("executing (useExecuteOnce)");
         page.setPageTitle("My page title");
-        page.addToHeader("link1", <link key="1" href="my-custom-style.css" rel="stylesheet"/>)
+        page.addStyleLinkToHeader("global", "my-custom-style.css");
     }, import.meta.filename);
 
     return <div>
@@ -20,5 +20,6 @@ export default function() {
         <div><Link to="/products">Products</Link></div>
         <div><Link to="/dont/exist">Page 404</Link></div>
         <div><Link to="/login">Login</Link></div>
+        <Button variant="outline">Click me</Button>
     </div>
 }
