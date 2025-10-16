6ac4f992500ada149abfe736fd329833c38067c5
Johan PIQUET <johan.piquet@gmail.com>
2025-10-16 12:09:56 +0200

    Zod replaced by a lighter internal engine

--- src/routes/products/$product/details/index.page.tsx
+++ src/routes/products/$product/details/index.page.tsx
@@ -1,4 +1,4 @@
-import {isServerSide, usePageParams} from "jopi-rewrite/ui";
+import {isServerSide, usePageParams} from "jopi-rewrite/uikit";
 
 export default function() {
     let params = usePageParams();
