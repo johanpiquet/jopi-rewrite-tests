85445c0e251ca3fb7273b67d9f229a5a0ae1602f
Johan PIQUET <johan.piquet@gmail.com>
2025-10-17 11:03:13 +0200

    Added req.getBodyData

--- src/routes/index.server.tsx
+++ src/routes/index.server.tsx
@@ -3,7 +3,7 @@ import {RouteServerContext} from "jopi-rewrite";
 export default function(ctx: RouteServerContext) {
     ctx.onPOST(async req => {
         // Get the data to send to the server.
-        const data = await req.getReqData();
+        const data = await req.getBodyData();
         // Return it as-is, in JSON format.
         return req.jsonResponse(data);
     });
--- src/routes/login.server.tsx
+++ src/routes/login.server.tsx
@@ -2,7 +2,7 @@ import {type LoginPassword, RouteServerContext} from "jopi-rewrite";
 
 export default function(ctx: RouteServerContext) {
     ctx.onPOST(async req => {
-        const data = await req.getReqData({ignoreUrl: true});
+        const data = await req.getBodyData();
         console.log("Post data:", data);
 
         let authResult = await req.tryAuthWithJWT(data as LoginPassword);
